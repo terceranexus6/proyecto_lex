6235d395d8d54f4266fe8497abc8ea015227f8e0
Paula <terceranexus6@users.noreply.github.com>
2016-12-02 08:43:22 +0100

    Create sniffeo_rapido.sh

---
+++ sniffeo_rapido.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+
+echo "Guardando datos de sniffeo "
+
+touch loquesea.txt
+ettercap -T -i wlan0 > sniffeo.txt &
+
+echo -ne '[->                        ]  '
+sleep 1
+echo -e '\0033\0143'
+echo "Guardando datos de sniffeo"
+echo -ne '[--->                      ]  '
+sleep 5
+echo -e '\0033\0143'
+echo "Guardando datos de sniffeo"
+echo -ne '[-------->                 ]  '
+sleep 1
+echo -e '\0033\0143'
+echo "Guardando datos de sniffeo"
+echo -ne '[------------->            ]   '
+sleep 1
+echo -e '\0033\0143'
+echo "Guardando datos de sniffeo"
+echo -ne '[---------------------->   ]'
+sleep 1
+echo -e '\0033\0143'
+echo "Guardando datos de sniffeo"
+echo -ne '[------------------------->]'
+echo " "
+
+echo "escaneo completo"
+kill $!
+
+./proyecto sniffeo_r.txt
